397f6b76eaa952f275277cb2cf40f0add30dbb89
Jen Tran <j.yn.tran@gmail.com>
2017-03-15 23:50:18 -0400

    Added test files

---
+++ portfolio/imports/api/projects.tests.js
@@ -0,0 +1,11 @@
+/* esline-env mocha*/
+
+import { Meteor } from 'meteor/meteor';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+import { Projects } from './projects.js';
+
+if (Meteor.isServer) {
+	describe('Projects', () => {
+	});
+}
