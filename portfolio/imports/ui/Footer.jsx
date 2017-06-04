fd62e1bd08ec2e5f33c788ef4b028ac7a2c5f164
J. Tran <j.yn.tran@gmail.com>
2017-06-04 00:34:31 +0000

    Add changes to navigation and footer on larger resolutions. Add logo SVG. Remove old file.

--- portfolio/imports/ui/Footer.jsx
+++ portfolio/imports/ui/Footer.jsx
@@ -4,12 +4,14 @@ export default class Footer extends Component {
 	render() {
 	return (
 		<div className="footer">
-			<div className="copy w50 float-left">
-				<p className="small">&copy; 2017 Jensen Tran</p>
-			</div>
-			<div className="social w50 float-right">
-				<a href="#"><i className="fa fa-facebook social-icon" aria-hidden="true"></i></a>
-				<a href="#"><i className="fa fa-twitter social-icon" aria-hidden="true"></i></a>
+			<div className="footer-container">
+				<div className="copy w50 float-left">
+					<p className="small">&copy; 2017 Jensen Tran</p>
+				</div>
+				<div className="social w50 float-right">
+					<a href="#"><i className="fa fa-facebook social-icon" aria-hidden="true"></i></a>
+					<a href="#"><i className="fa fa-twitter social-icon" aria-hidden="true"></i></a>
+				</div>
 			</div>
 		</div>
 	)
