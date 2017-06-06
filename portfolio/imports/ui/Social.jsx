e736398acea59e88eeff97cce590b5521cdc5466
J. Tran <j.yn.tran@gmail.com>
2017-06-06 16:14:07 +0000

    Set up Meteor settings on client-side too

--- portfolio/imports/ui/Social.jsx
+++ portfolio/imports/ui/Social.jsx
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 
-import URLs from './URLs.jsx';
-
 export default class SocialSection extends Component {
 	render() {
-	return (
-		<div className="social">
-			<a href={URLs.github} title="GitHub">
-				<i className="fa fa-github" aria-hidden="true"></i>
-			</a>
-			<a href={URLs.linkedin} title="LinkedIn">
-				<i className="fa fa-linkedin" aria-hidden="true"></i>
-			</a>
-			<a href={URLs.twitter} title="Twitter">
-				<i className="fa fa-twitter" aria-hidden="true"></i>
-			</a>
-			<a href={URLs.facebook} title="Facebook">
-				<i className="fa fa-facebook" aria-hidden="true"></i>
-			</a>
-		</div>
-	);
+		const githubURL = Meteor.settings.public.github;
+		const linkedinURL = Meteor.settings.public.linkedin;
+		const twitterURL = Meteor.settings.public.twitter;
+		const facebookURL = Meteor.settings.public.facebook;
+
+		return (
+			<div className="social">
+				<a href={githubURL} title="GitHub">
+					<i className="fa fa-github" aria-hidden="true"></i>
+				</a>
+				<a href={linkedinURL} title="LinkedIn">
+					<i className="fa fa-linkedin" aria-hidden="true"></i>
+				</a>
+				<a href={twitterURL} title="Twitter">
+					<i className="fa fa-twitter" aria-hidden="true"></i>
+				</a>
+				<a href={facebookURL} title="Facebook">
+					<i className="fa fa-facebook" aria-hidden="true"></i>
+				</a>
+			</div>
+		);
 	}
 }
--- portfolio/imports/ui/URLs.jsx
+++
@@ -1,9 +0,0 @@
-export default {
-	resume: "dl/jensen-tran-cv.pdf",
-	github: "https://github.com/jyntran",
-	linkedin: "https://www.linkedin.com/in/jyntran",
-	twitter: "https://twitter.com/jyntran",
-	facebook: "https://www.facebook.com/jyntran",
-	blog: "http://blog.jyntran.ca",
-	lastfm: "https://last.fm/jyntran"
-}
